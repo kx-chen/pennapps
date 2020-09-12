f8a75e3b7b09c43698beeb36084e0c2dd4447a5a
jonathanzhang53 <jonathanzhang53@gmail.com>
2020-09-12 11:42:52 -0500

    api get response-list and question-list calls

---
+++ djangoapp/api/README.md
@@ -0,0 +1,10 @@
+# Interview App REST API
+
+### Rest Framework
+* `pip install djangorestframework`
+
+### Quickstart
+* `cd djangoapp`
+* `python manage.py runserver`
+* Visit `http://127.0.0.1:8000/api/`
+* Visit `http://127.0.0.1:8000/admin/`
--- djangoapp/api/urls.py
+++ djangoapp/api/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.apiOverview, name="api-overview")
+    path('', views.apiOverview, name="api-overview"),
+    path('question-list/', views.interviewQuestionList, name="question-list"),
+    path('response-list/', views.interviewResponseList, name="response-list"),
+    path('question-detail/<str:pk>', views.interviewQuestionDetail, name="question-detail"),
+    path('response-detail/<str:pk>', views.interviewResponseDetail, name="response-detail"),
 ]
