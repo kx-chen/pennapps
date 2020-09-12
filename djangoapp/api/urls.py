304522111325bf93a7b2dbd3868a42f92d6dc364
jonathanzhang53 <jonathanzhang53@gmail.com>
2020-09-12 12:10:02 -0500

    api crud

--- djangoapp/api/urls.py
+++ djangoapp/api/urls.py
@@ -3,13 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
-<<<<<<< HEAD
     path('question-list/', views.interviewQuestionList, name="question-list"),
     path('response-list/', views.interviewResponseList, name="response-list"),
-    path('question-detail/<str:pk>', views.interviewQuestionDetail, name="question-detail"),
-    path('response-detail/<str:pk>', views.interviewResponseDetail, name="response-detail"),
-=======
-    path('response-list/', views.interviewResponseList, name="response-list"),
-    path('question-list/', views.interviewQuestionList, name="question-list")
->>>>>>> upstream/master
+    path('question-detail/<str:pk>/', views.interviewQuestionDetail, name="question-detail"),
+    path('response-detail/<str:pk>/', views.interviewResponseDetail, name="response-detail"),
+    path('response-create/', views.interviewResponseCreate, name="response-create"),
+    path('response-update/<str:pk>/', views.interviewResponseUpdate, name="response-update"),
+    path('response-delete/<str:pk>/', views.interviewResponseDelete, name="response-delete"),
 ]
--- djangoapp/api/views.py
+++ djangoapp/api/views.py
@@ -22,6 +22,18 @@ def apiOverview(request):
     return Response(api_urls)
 
 @api_view(["GET"])
+def interviewQuestionList(request):
+    questions = InterviewQuestion.objects.all()
+    serializer = InterviewQuestionSerializer(questions, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def interviewQuestionDetail(request, pk):
+    question = InterviewQuestion.objects.get(id=pk)
+    serializer = InterviewQuestionSerializer(question, many=False)
+    return Response(serializer.data)
+
+@api_view(["GET"])
 def interviewResponseList(request):
     responses = InterviewResponse.objects.all()
     serializer = InterviewResponseSerializer(responses, many=True)
@@ -33,14 +45,23 @@ def interviewResponseDetail(request, pk):
     serializer = InterviewResponseSerializer(response, many=False)
     return Response(serializer.data)
 
-@api_view(["GET"])
-def interviewQuestionList(request):
-    questions = InterviewQuestion.objects.all()
-    serializer = InterviewQuestionSerializer(questions, many=True)
+@api_view(["POST"])
+def interviewResponseCreate(request):
+    serializer = InterviewResponseSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
     return Response(serializer.data)
 
-@api_view(["GET"])
-def interviewQuestionDetail(request, pk):
-    question = InterviewQuestion.objects.get(id=pk)
-    serializer = InterviewQuestionSerializer(question, many=False)
+@api_view(["POST"])
+def interviewResponseUpdate(request, pk):
+    response = InterviewResponse.objects.get(id=pk)
+    serializer = InterviewResponseSerializer(instance=response, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
     return Response(serializer.data)
+
+@api_view(["DELETE"])
+def interviewResponseDelete(request, pk):
+    response = InterviewResponse.objects.get(id=pk)
+    response.delete()
+    return Response("successfully deleted")
