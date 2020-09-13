cadad95e3352e4c03fb40196d6aa2debca81b687
jonathanzhang53 <jonathanzhang53@gmail.com>
2020-09-13 00:44:04 -0500

    parsing and small changes

--- djangoapp/api/helpers.py
+++ djangoapp/api/helpers.py
@@ -1,12 +1,18 @@
 from textblob import TextBlob
+from nltk.corpus import words
 import nltk
+import re
 
 def analyze(data):
+    # WORDS PER MINUTE (WPM)
+    response = re.sub(r"[^\w\d'\s]", '', data["response"])
+    # print(response)
     minutes = int(data["time"]) / 60
-    words = data["response"].split()
-    wpm = len(words) // minutes
+    responsewords = response.split()
+    wpm = len(responsewords) // minutes
     data["wpm"] = wpm
 
+    # HESITATION FORMS ("um"s)
     filler = {
         "um": 0, 
         "like": 0, 
@@ -17,16 +23,34 @@ def analyze(data):
         "er": 0,
         "ah": 0,
     }
-    for i in range(len(words)):
-        word = words[i].lower()
+    for i in range(len(responsewords)):
+        word = responsewords[i].lower()
         if word in filler:
             filler[word] += 1
     data["um"] = ":".join("{};{}".format(k,v) for k,v in filler.items())
 
+    # NLP KEY PHRASES (noun-phrases)
     blob = TextBlob(data["response"])
     phrases = blob.noun_phrases
-    data["keyphrases"] = ",".join(phrases)
+    # print(phrases)
+    i = 0
+    nltk.download('words')
+    wordlist = set(words.words())
+    while i < len(phrases):
+        phrase = str(phrases[i]).split()
+        for j in range(len(phrase)):
+            if phrase[j] in wordlist:
+                continue
+            else:
+                phrases.pop(i)
+                i -= 1
+        i += 1
+    if phrases:
+        data["keyphrases"] = ",".join(phrases)
+    else:
+        data["keyphrases"] = "No key phrases were detected."
 
+    # QUESTION ID KEY
     data["q"] = int(data["q"])
 
     return data
--- djangoapp/api/views.py
+++ djangoapp/api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
 
 from .serializers import InterviewResponseSerializer, InterviewQuestionSerializer
 from .models import InterviewResponse, InterviewQuestion
@@ -47,8 +48,9 @@ def interviewResponseDetail(request, pk):
     return Response(serializer.data)
 
 @api_view(["POST"])
+@parser_classes([MultiPartParser])
 def interviewResponseCreate(request):
-    # print(request.data) 
+    # print(request.data)
     # {'response': AUDIO FILE BINARY, 
     # 'time': INTEGER TIME, 
     # 'q': INTEGER QUESTION ID KEY}
